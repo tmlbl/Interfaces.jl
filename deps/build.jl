5b435c49d0459081fb2a6e7df02cb385b8c872eb
tmlbl <tim.lebel@gmail.com>
2015-07-26 10:27:56 -0700

    Fix include paths on latest Julia master

--- deps/build.jl
+++ deps/build.jl
@@ -5,10 +5,11 @@ This directory should contain an include and lib directories.
 BUILDFILE_PATH = @__FILE__
 DEPS_PATH = dirname(BUILDFILE_PATH)
 
-JULIA_INCLUDE = "$(JULIA_HOME)/../include/julia"
-JULIA_LIB = "$(JULIA_HOME)/../lib/julia"
+JULIA_INCLUDE = "$(JULIA_HOME)/../include"
+JULIA_SRC = "$(JULIA_HOME)/../../src"
+JULIA_LIB = "$(JULIA_HOME)/../lib"
 
 mkpath("$(DEPS_PATH)/usr/lib/")
 
-run(`gcc -c -Wall -Werror -fPIC -I$(JULIA_INCLUDE) $(DEPS_PATH)/src/mutable_union.c -o $(DEPS_PATH)/usr/lib/mutable_union.o`)
+run(`gcc -c -Wall -Werror -fPIC -I$(JULIA_INCLUDE) -I$(JULIA_SRC) -I $(JULIA_SRC)/support $(DEPS_PATH)/src/mutable_union.c -o $(DEPS_PATH)/usr/lib/mutable_union.o`)
 run(`gcc -shared -ljulia -L$(JULIA_LIB) -o $(DEPS_PATH)/usr/lib/lib_mutable_union.so $(DEPS_PATH)/usr/lib/mutable_union.o`)
